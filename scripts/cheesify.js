94b4e795153a3a4a651816d072bdeff582c621e6
Jiayi <18341624+xujiayi1256@users.noreply.github.com>
2021-08-26 15:36:41 +0800

    Update

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,11 +1,31 @@
-document.querySelector(".AppHeader").style.display='none';
+if (document.querySelector(".AppHeader")) {
+    document.querySelector(".AppHeader").style.display='none';
+};
 
-document.querySelector(".GlobalSideBar .Sticky").style.display='none';
+if (document.querySelector(".GlobalSideBar")) {
+    document.querySelector(".GlobalSideBar").style.display='none';
+};
 
-document.querySelector(".Topstory-tabCard").style.display='none';
+if (document.querySelector(".Question-sideColumn")) {
+    document.querySelector(".Question-sideColumn").style.display='none';
+};
 
-document.querySelectorAll(".Pc-feedAd-container").forEach((ele)=>{
-    ele.style.display='none';
-});
+if (document.querySelector(".Topstory-tabCard")) {
+    document.querySelector(".Topstory-tabCard").style.display='none';
+};
 
-document.querySelector(".Topstory-mainColumn").style.width="60rem";
+if (document.querySelector(".Topstory-mainColumn")) {
+    document.querySelector(".Topstory-mainColumn").style.width="60rem";
+};
+
+if (document.querySelector(".Question-mainColumn")) {
+    document.querySelector(".Question-mainColumn").style.width="60rem";
+};
+
+setTimeout("hideAd()", 2000);
+
+function hideAd() {
+    document.querySelectorAll(".TopstoryItem--advertCard").forEach((ele)=>{
+        ele.style.display='none';
+    });
+};
