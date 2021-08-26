165ba2ce257be9da52329981e69f96c18709c97e
Jiayi <18341624+xujiayi1256@users.noreply.github.com>
2021-08-26 11:33:44 +0800

    Upload

---
+++ scripts/cheesify.js
@@ -0,0 +1,11 @@
+document.querySelector(".AppHeader").style.display='none';
+
+document.querySelector(".GlobalSideBar .Sticky").style.display='none';
+
+document.querySelector(".Topstory-tabCard").style.display='none';
+
+document.querySelectorAll(".Pc-feedAd-container").forEach((ele)=>{
+    ele.style.display='none';
+});
+
+document.querySelector(".Topstory-mainColumn").style.width="60rem";
